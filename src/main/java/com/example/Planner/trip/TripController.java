bd3158a08e39db44493977299a9f73a9339c84c5
Filipe Douglas <felipe.douglas.inf1@gmail.com>
2024-07-11 11:12:19 -0300

    Retirando linhas de códigos não usadas

--- src/main/java/com/example/Planner/trip/TripController.java
+++ src/main/java/com/example/Planner/trip/TripController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.Planner.participant.Participant;
 import com.example.Planner.participant.ParticipantData;
 import com.example.Planner.participant.ParticipantRequestDTO;
 import com.example.Planner.participant.ParticipantResponseDTO;
